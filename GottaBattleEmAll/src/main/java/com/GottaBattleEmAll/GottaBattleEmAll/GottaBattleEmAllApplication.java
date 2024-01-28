01d776dc2c38b69fbeceebf483bc138ea64f40bf
Nakura125 <angeloalberico00@gmail.com>
2024-01-28 12:06:19 +0100

    First create

---
+++ GottaBattleEmAll/src/main/java/com/GottaBattleEmAll/GottaBattleEmAll/GottaBattleEmAllApplication.java
@@ -0,0 +1,13 @@
+package com.GottaBattleEmAll.GottaBattleEmAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GottaBattleEmAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GottaBattleEmAllApplication.class, args);
+	}
+
+}
