e21d662f35011b1e4757a62abae22c0f99bac625
Gianvi-lan <127248735+Gianvi-lan@users.noreply.github.com>
2024-02-03 23:04:07 +0100

    add GuestController and fix registrazione.html (WIP)

--- GottaBattleEmAll/src/main/java/com/GottaBattleEmAll/GottaBattleEmAll/controller/GuestController.java
+++ GottaBattleEmAll/src/main/java/com/GottaBattleEmAll/GottaBattleEmAll/controller/GuestController.java
@@ -1,18 +1,71 @@
 package com.GottaBattleEmAll.GottaBattleEmAll.controller;
 
+import com.GottaBattleEmAll.GottaBattleEmAll.entity.Giocatore;
+import com.GottaBattleEmAll.GottaBattleEmAll.entity.Organizzatore;
+import com.GottaBattleEmAll.GottaBattleEmAll.service.GuestService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Enumeration;
 
 @Controller
 public class GuestController {
 
-    @GetMapping("/registrazione")
-    public String registrazione( Model model) {
-        return "registrazione";
+    private final GuestService guestService;
+
+    @Autowired
+    public GuestController(GuestService guestService){
+        this.guestService = guestService;
     }
 
 
+    @GetMapping("/registrazione")
+    public String registrazione(Model model) {
+        return "registrazione";
+    }
+
+    @PostMapping("/registrazione")
+    public String registrazione(@ModelAttribute Giocatore giocatore,
+                                @ModelAttribute Organizzatore organizzatore,
+                                @RequestParam("confermaPassword") String confermaPassword,
+                                @RequestParam("ruolo") String ruolo,
+                                Model model) {
+
+/*        System.out.println("Ruolo: " + ruolo);
+
+        System.out.println("Giocatore: " +
+                "Username: " + giocatore.getUsername() +
+                ", Nome: " + giocatore.getNome() +
+                ", Cognome: " + giocatore.getCognome() +
+                ", Email: " + giocatore.getEmail());
+
+        System.out.println("Organizzatore: " +
+                "Username: " + organizzatore.getUsername() +
+                ", Nome: " + organizzatore.getNome() +
+                ", Cognome: " + organizzatore.getCognome() +
+                ", Email: " + organizzatore.getEmail());
+
+        System.out.println("Conferma password: " + confermaPassword);*/
 
 
+        if ("giocatore".equals(ruolo)) {
+            // Chiamare il servizio di registrazione del giocatore come utente
+            String risultatoRegistrazione = guestService.registrazioneGiocatore(giocatore, confermaPassword);
+            model.addAttribute("risultatoRegistrazione", risultatoRegistrazione);
+            System.out.println("Risultato registrazione: " + risultatoRegistrazione);
+        } else if ("organizzatore".equals(ruolo)) {
+            // Chiamare il servizio di registrazione dell'organizzatore
+            String risultatoRegistrazione = guestService.registrazioneOrganizzatore(organizzatore, confermaPassword);
+            model.addAttribute("risultatoRegistrazione", risultatoRegistrazione);
+            System.out.println("Risultato registrazione: " + risultatoRegistrazione);
+        }
+
+
+        return "registrazione";
+    }
 }
+
+
