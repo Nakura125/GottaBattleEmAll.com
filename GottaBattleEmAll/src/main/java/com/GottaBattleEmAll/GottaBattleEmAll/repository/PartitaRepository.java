6619c26303660e56e77aaa60b28c94e3d7cf9eec
LuisSteel <luigirocchinoo@gmail.com>
2024-02-06 15:28:45 +0100

    Add PartitaServiceImpl + PartitaTest

--- GottaBattleEmAll/src/main/java/com/GottaBattleEmAll/GottaBattleEmAll/repository/PartitaRepository.java
+++ GottaBattleEmAll/src/main/java/com/GottaBattleEmAll/GottaBattleEmAll/repository/PartitaRepository.java
@@ -4,8 +4,10 @@ import com.GottaBattleEmAll.GottaBattleEmAll.entity.Partita;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 
 public interface PartitaRepository extends JpaRepository<Partita, UUID> {
+
 }
